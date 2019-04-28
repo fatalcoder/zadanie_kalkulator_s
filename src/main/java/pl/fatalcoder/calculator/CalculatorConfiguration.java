a2804a75eae1f1dda956798112e3bb673b7951e8
Dawid Woźniak <dawid.wozniak@pearson.com>
2019-04-28 17:43:50 +0200

    Access-Control-Allow-Origin: *

--- src/main/java/pl/fatalcoder/calculator/CalculatorConfiguration.java
+++ src/main/java/pl/fatalcoder/calculator/CalculatorConfiguration.java
@@ -1,4 +1,20 @@
 package pl.fatalcoder.calculator;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CalculatorConfiguration {
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+      }
+    };
+  }
 }
